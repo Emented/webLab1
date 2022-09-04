3c10c74839a343728070670f13c4a377bd667cb5
Ilya <ilerys61@gmail.com>
2022-09-04 18:28:27 +0300

    Implement server side validation

--- src/php/index.php
+++ src/php/index.php
@@ -1,50 +1,81 @@
 <?php
-class Validator {
+class Validator
+{
+    function check_data_type($x, $y, $r)
+    {
+        if (!is_numeric($x) || !is_numeric($y) || !is_numeric($r)) {
+            http_response_code(404);
+            exit("Some values are not numeric!");
+        }
+    }
 
+    function check_aceptability($x, $y, $r)
+    {
+        if (!isset($x) || !isset($y) || !isset($r)) {
+            http_response_code(404);
+            exit("Some values are not set in the request!");
+        }
+    }
+
+    public function check_values($x, $y, $r)
+    {
+        $this->check_aceptability($x, $y, $r);
+        $this->check_data_type($x, $y, $r);
+    }
+}
+
+class HitChecker
+{
+    public function check($x, $y, $r)
+    {
+        $x_num = intval($x);
+        $y_num = floatval($y);
+        $r_num = intval($r);
+        $first_quarter_hit = false;
+        $third_quarter_hit = false;
+        $fourth_quarter_hit = false;
+
+        if ($x_num >= 0 && $x_num <= $r_num / 2 && $y_num >= 0 && $y_num <= $r_num) {
+            $first_quarter_hit = true;
+        }
+
+        if ($x_num <= 0 && $y_num <= 0 && $x_num * $x_num + $y_num * $y_num <= $r_num * $r_num) {
+            $third_quarter_hit = true;
+        }
+
+        if ($x_num >= 0 && $y_num <= 0 && $y_num >= $x_num / 2 - $r_num) {
+            $fourth_quarter_hit = true;
+        }
+
+        return $first_quarter_hit || $third_quarter_hit || $fourth_quarter_hit;
+    }
 }
 
 date_default_timezone_set('Europe/Moscow');
 
-function check_hit($x, $y, $r)
-{
-    $first_quarter_hit = false;
-    $third_quarter_hit = false;
-    $fourth_quarter_hit = false;
-
-    if ($x >= 0 && $x <= $r / 2 && $y >= 0 && $y <= $r) {
-        $first_quarter_hit = true;
-    }
-
-    if ($x <= 0 && $y <= 0 && $x * $x + $y * $y <= $r * $r) {
-        $third_quarter_hit = true;
-    }
-
-    if ($x >= 0 && $y <= 0 && $y >= $x / 2 - $r) {
-        $fourth_quarter_hit = true;
-    }
-
-    return $first_quarter_hit || $third_quarter_hit || $fourth_quarter_hit;
-}
-
 $start = microtime(true);
 
 $current_time = date("H:i:s");
-if (isset($_POST["x"]) && isset($_POST["y"]) && isset($_POST["r"])) {
-    $x = intval($_POST["x"]);
-    $y = floatval($_POST["y"]);
-    $r = intval($_POST["r"]);
 
-    $checked_hit = check_hit($x, $y, $r) ? "TRUE" : "FALSE";
+$x = $_POST['x'];
+$y = $_POST['y'];
+$r = $_POST['r'];
 
-    $compution_time = number_format(microtime(true) - $start, 8, ".", "") * 1000000;
+$validator = new Validator();
+$hit_checker = new HitChecker();
 
-    exit("
-            <tr>
-                <th><time>$current_time</time></th>
-                <th><time>$compution_time</time></th>
-                <th>$x</th>
-                <th>$y</th>
-                <th>$r</th>
-                <th>$checked_hit</th>
-            </tr>");
-}
+$validator->check_values($x, $y, $r);
+
+$checked_hit = $hit_checker->check($x, $y, $r) ? "TRUE" : "FALSE";
+
+$compution_time = number_format(microtime(true) - $start, 8, ".", "") * 1000000;
+
+exit("
+        <tr>
+            <th><time>$current_time</time></th>
+            <th><time>$compution_time</time></th>
+            <th>$x</th>
+            <th>$y</th>
+            <th>$r</th>
+            <th>$checked_hit</th>
+        </tr>");
